8b9c892052e4e6330dce7276601925d080da8479
saviorikelme <savio.rikelme@gmail.com>
2026-03-31 17:56:38 -0300

    Create attA.c

--- lista4/attA.c
+++ lista4/attA.c
@@ -1,33 +1,25 @@
 #include <stdio.h>
 
-// Função para verificar se a posição é válida
+// Função para validar se a posição é segura para a Rebeka
+// Em C, matrizes passadas para funções precisam das dimensões (l e c)
 int eh_valido(int x, int y, int l, int c, char mapa[l][c], int visitado[l][c]) {
-    if (x >= 0 && x < l && y >= 0 && y < c && mapa[x][y] != '#' && !visitado[x][y]) {
+    if (x >= 0 && x < l && y >= 0 && y < c && mapa[x][y] != '#' && visitado[x][y] == 0) {
         return 1;
     }
     return 0;
 }
 
-// Função para gerenciar a inserção na fila
-void enfileirar(int x, int y, int d, int *fim, int filaX[], int filaY[], int filaDist[]) {
-    filaX[*fim] = x;
-    filaY[*fim] = y;
-    filaDist[*fim] = d;
-    (*fim)++; // Incrementa o contador do fim da fila usando ponteiro
-}
-
 int main() {
     int linhas, colunas;
 
-    // Leitura do formato "3x4"
+    // Leitura do formato "linhasxcolunas" (ex: 3x4)
     if (scanf("%dx%d", &linhas, &colunas) != 2) return 0;
 
-    // Declaração local das matrizes
     char mapa[linhas][colunas];
     int visitado[linhas][colunas];
     int startX, startY;
 
-    // Leitura do mapa
+    // Lendo o mapa e limpando a matriz de visitados
     for (int i = 0; i < linhas; i++) {
         for (int j = 0; j < colunas; j++) {
             scanf(" %c", &mapa[i][j]);
@@ -39,41 +31,55 @@ int main() {
         }
     }
 
-    // Fila local (tamanho baseado na área total do mapa)
+    // Fila manual usando vetores paralelos
     int tam_max = linhas * colunas;
     int filaX[tam_max], filaY[tam_max], filaDist[tam_max];
     int inicio = 0, fim = 0;
 
-    // Início da busca
-    enfileirar(startX, startY, 0, &fim, filaX, filaY, filaDist);
+    // Adiciona a origem na fila (Enfileirar manual para evitar ponteiros)
+    filaX[fim] = startX;
+    filaY[fim] = startY;
+    filaDist[fim] = 0;
+    fim++;
     visitado[startX][startY] = 1;
 
     int menorDistancia = -1;
+    
+    // Vetores de direção: Cima, Baixo, Esquerda, Direita
     int dx[] = {-1, 1, 0, 0};
     int dy[] = {0, 0, -1, 1};
 
+    // BFS - Busca em Largura
     while (inicio < fim) {
         int xAtual = filaX[inicio];
         int yAtual = filaY[inicio];
         int dAtual = filaDist[inicio];
         inicio++;
 
+        // Se achou a saída 'd'
         if (mapa[xAtual][yAtual] == 'd') {
             menorDistancia = dAtual;
             break;
         }
 
+        // Tenta os 4 movimentos usando a função auxiliar
         for (int i = 0; i < 4; i++) {
-            int px = xAtual + dx[i];
-            int py = yAtual + dy[i];
+            int nx = xAtual + dx[i];
+            int ny = yAtual + dy[i];
 
-            if (eh_valido(px, py, linhas, colunas, mapa, visitado)) {
-                visitado[px][py] = 1;
-                enfileirar(px, py, dAtual + 1, &fim, filaX, filaY, filaDist);
+            if (eh_valido(nx, ny, linhas, colunas, mapa, visitado)) {
+                visitado[nx][ny] = 1;
+                
+                // Enfileirar manual
+                filaX[fim] = nx;
+                filaY[fim] = ny;
+                filaDist[fim] = dAtual + 1;
+                fim++;
             }
         }
     }
 
+    // Resultado final
     if (menorDistancia != -1) {
         printf("Apos correr %d metros e quase desistir por causa da distância, Rebeka conseguiu escapar!\n", menorDistancia);
     } else {
